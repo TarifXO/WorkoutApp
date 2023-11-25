321574984a8c498ea7ed89f67285311a18e4059f
Tarif <abdo.tareef10@gmail.com>
2023-11-25 15:39:08 +0200

    updated

--- app/src/main/java/com/example/workout/HistoryActivity.kt
+++ app/src/main/java/com/example/workout/HistoryActivity.kt
@@ -3,7 +3,6 @@ package com.example.workout
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
